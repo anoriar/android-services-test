a2b806c35f2873d36ce96c98bb5278eed884506b
borisov.alex12@gmail.com <borisov.alex12@gmail.com>
2022-03-28 16:55:20 +0300

    job service

---
+++ app/src/main/java/ru/sumin/servicestest/MyJobService.kt
@@ -0,0 +1,44 @@
+package ru.sumin.servicestest
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyJobService : JobService() {
+
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartJob")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("$i seconds")
+            }
+            jobFinished(params, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        coroutineScope.cancel()
+    }
+
+    private fun log(message: String) {
+        Log.d("TEST_SERVICE", "Job Service: $message")
+    }
+}
